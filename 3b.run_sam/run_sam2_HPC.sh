2b847cbc263dcaf1dc088db772299e36015b0d90
MikeLippincott <1michaell2017@gmail.com>
2024-09-18 13:28:26 -0600

    run sam2 on HPC

--- 3b.run_sam/run_sam2_HPC.sh
+++ 3b.run_sam/run_sam2_HPC.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --time=6:00:00
+#SBATCH --partition=aa100
+#SBATCH --gres=gpu:2
+#SBATCH --output=sample-%j.out
+
+
 # run SAM2
 echo "Starting SAM2 pipe for object detection..."
 
 # run SAM2
-
+module load cuda
+module load anaconda
+module load mambaforge
+conda init bash
 mamba activate sam2_env
 
 jupyter nbconvert --to python --output-dir=scripts/ notebooks/*.ipynb
