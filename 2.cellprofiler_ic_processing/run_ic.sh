183bf09d46e0a5639faf30d78ce1787deaadfb09
MikeLippincott <1michaell2017@gmail.com>
2024-12-30 10:02:38 -0700

    fix slurm scripts

--- 2.cellprofiler_ic_processing/run_ic.sh
+++ 2.cellprofiler_ic_processing/run_ic.sh
@@ -4,14 +4,15 @@
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
 
 # change the directory to the scripts folder
-cd scripts/
+cd scripts/ || exit
 
 echo "Starting IC processing"
 
 # Run CellProfiler for IC processing
-conda run -n cellprofiler_timelapse_env python perform_ic.py
+conda run -n cellprofiler_timelapse_env python 0.perform_ic.py --test_data True
+conda run -n cellprofiler_timelapse_env python 1.process_ic_teminal_data.py --test_data True
 
 # return the directory that script was run from
-cd ../
+cd ../ || exit
 
 echo "IC processing complete"
--- 2.cellprofiler_ic_processing/run_ic_HPC.sh
+++ 2.cellprofiler_ic_processing/run_ic_HPC.sh
@@ -19,10 +19,10 @@ cd scripts/ || exit
 echo "Starting IC processing"
 
 # Run CellProfiler for IC processing
-conda run -n cellprofiler_timelapse_env python 0.perform_ic.py
-conda run -n cellprofiler_timelapse_env python 1.process_ic_teminal_data.py
+conda run -n cellprofiler_timelapse_env python 0.perform_ic.py --test_data True
+conda run -n cellprofiler_timelapse_env python 1.process_ic_teminal_data.py --test_data True
 
 # return the directory that script was run from
-cd ../
+cd ../ || exit
 
 echo "IC processing complete"
