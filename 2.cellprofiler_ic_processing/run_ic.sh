821e09e2de3e4929da9e43d3f5ed4d490c13ea40
MikeLippincott <1michaell2017@gmail.com>
2025-01-03 12:16:16 -0700

    run on HPC

--- 2.cellprofiler_ic_processing/run_ic.sh
+++ 2.cellprofiler_ic_processing/run_ic.sh
@@ -9,8 +9,8 @@ cd scripts/ || exit
 echo "Starting IC processing"
 
 # Run CellProfiler for IC processing
-conda run -n cellprofiler_timelapse_env python 0.perform_ic.py --test_data True
-conda run -n cellprofiler_timelapse_env python 1.process_ic_teminal_data.py --test_data True
+conda run -n cellprofiler_timelapse_env python 0.perform_ic.py # --test_data True
+conda run -n cellprofiler_timelapse_env python 1.process_ic_teminal_data.py # --test_data
 
 # return the directory that script was run from
 cd ../ || exit
