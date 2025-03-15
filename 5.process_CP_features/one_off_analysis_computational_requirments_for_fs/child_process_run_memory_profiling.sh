da2db3efdc8af382b70bba95d9d2796f7f03fed8
MikeLippincott <1michaell2017@gmail.com>
2025-03-15 13:02:38 -0600

    fix one off

--- 5.process_CP_features/one_off_analysis_computational_requirments_for_fs/child_process_run_memory_profiling.sh
+++ 5.process_CP_features/one_off_analysis_computational_requirments_for_fs/child_process_run_memory_profiling.sh
@@ -6,7 +6,7 @@ jupyter nbconvert --to python --output-dir=scripts/ notebooks/*.ipynb
 
 cd scripts/ || exit
 
-python 1.one_off_analysis_computational_requirments_for_fs.py --num_of_featuresq $1 --num_of_cells_per_well $2 --num_of_groups $3 --num_of_replicates $4
+python 1.one_off_analysis_computational_requirments_for_fs.py --num_of_features $1 --num_of_cells_per_well $2 --num_of_groups $3 --num_of_replicates $4
 
 cd ../ || exit
 
