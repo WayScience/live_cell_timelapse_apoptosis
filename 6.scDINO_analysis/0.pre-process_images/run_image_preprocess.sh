9120d58e8b6dccdd75a85a286f40cb78ee30ebb2
MikeLippincott <1michaell2017@gmail.com>
2025-03-21 09:23:01 -0600

    ready to run

--- 6.scDINO_analysis/0.pre-process_images/run_image_preprocess.sh
+++ 6.scDINO_analysis/0.pre-process_images/run_image_preprocess.sh
@@ -7,14 +7,14 @@ conda activate scDINO_env
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
 
 # change to the correct directory
-cd scripts
+cd scripts || exit
 
 # run the script
 time python 0.pre-process_images.py
 time python 1.calculate_mean_std_per_channel.py
 
 # revert to the original directory
-cd ..
+cd .. || exit
 
 # deactivate the env
 conda deactivate
