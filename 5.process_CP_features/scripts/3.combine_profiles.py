c36fcb87a284e4375c775bef13b0142353ab5947
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2025-03-31 20:07:37 -0600

    Rerun post profiling processing (#24)
    * progress capture
    
    * progress cpature for IBP
    
    * ready to run on HPC - IBP
    
    * rerun on HPC
    
    * fix the no cells error
    
    * fix one off
    
    * fix one off
    
    * fix one off
    
    * each ready to go
    
    * rerun fuzzy matching
    
    * update the gitignore
    
    * run ibp on timelapse
    
    * update gitignore
    
    * add scripts
    
    * Update 5.process_CP_features/scripts/3.combine_profiles.py
    
    Co-authored-by: Jenna Tomkinson <107513215+jenna-tomkinson@users.noreply.github.com>
    
    * Update 5.process_CP_features/scripts/6.aggregation.py
    
    Co-authored-by: Jenna Tomkinson <107513215+jenna-tomkinson@users.noreply.github.com>
    
    * addressing comments:
    
    ---------
    
    Co-authored-by: Jenna Tomkinson <107513215+jenna-tomkinson@users.noreply.github.com>

---
+++ 5.process_CP_features/scripts/3.combine_profiles.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Combine annotated tracked profiles into one parquet file
+
+# ## Import libraries
+
+# In[1]:
+
+
+import pathlib
+
+import pandas as pd
+
+# ## Set paths and variables
+
+# In[2]:
+
+
+# set paths
+paths_dict = {
+    "timelapse_profiles": {
+        "input_dir": pathlib.Path("../data/2.sc_tracks_annotated_data/profiles"),
+        "output_file_dir": pathlib.Path(
+            "../data/3.combined_data/profiles/combined_data.parquet"
+        ),
+    },
+    "endpoint_data": {
+        "input_dir": pathlib.Path("../data/1.annotated_data/endpoint"),
+        "output_file_dir": pathlib.Path(
+            "../data/3.combined_data/endpoints/combined_data.parquet"
+        ),
+    },
+}
+
+
+# In[3]:
+
+
+for data_set in paths_dict.keys():
+    files = list(paths_dict[data_set]["input_dir"].rglob("*.parquet"))
+    print(f"Found {len(files)} files in {data_set} directory")
+    list_of_dfs = [pd.read_parquet(file) for file in files]
+    combined_df = pd.concat(list_of_dfs, ignore_index=True)
+    paths_dict[data_set]["output_file_dir"].parent.mkdir(parents=True, exist_ok=True)
+    combined_df.to_parquet(paths_dict[data_set]["output_file_dir"])
+    print(combined_df.shape)
